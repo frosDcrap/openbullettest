4f6af5ecbf48fe8f981ff278874e95fc555dab61
BuildTools <unconfigured@null.spigotmc.org>
2021-04-25 17:16:08 -0700

    lez go'

---
+++ Docs/lolicode/statements.md
@@ -0,0 +1,110 @@
+﻿### LoliCode specific statements
+
+##### LOG
+Prints text to the debugger log.
+Example:
+```
+LOG "hello"
+```
+---
+##### CLOG
+Prints colored text to the debugger log.
+A full list of colors is available [here](https://www.colorhexa.com/color-names) (remove dashes and spaces and apply PascalCase).
+Example:
+```
+CLOG YellowGreen "hello"
+```
+---
+##### JUMP
+Jumps to a specified point in the code. Remember to watch out for endless loops!
+Example:
+```
+...
+#HERE
+...
+JUMP #HERE
+```
+---
+##### REPEAT
+Repeats something N times.
+Example:
+```
+REPEAT 5
+LOG "hello"
+END
+```
+---
+##### FOREACH
+Iterates on a list variable.
+Example:
+```
+BLOCK:ConstantList
+  value = ["one", "two", "three"]
+  => VAR @list
+ENDBLOCK
+
+FOREACH elem IN list
+LOG elem
+END
+```
+---
+##### WHILE
+Executes something while a condition is true.
+Example:
+```
+WHILE INTKEY 1 LessThan 2
+...
+END
+```
+---
+##### IF / ELSE / ELSE IF
+Executes something, or something else.
+Example:
+```
+IF INTKEY 5 LessThan 1
+LOG "nope"
+ELSE IF INTKEY 5 LessThan 3
+LOG "nope again"
+ELSE
+LOG "yep"
+END
+```
+---
+##### TRY / CATCH
+Executes something. If it fails, executes something else.
+Example:
+```
+TRY
+// request to an unreliable URL
+CATCH
+// fallback request to a reliable URL
+END
+```
+---
+##### LOCK
+Very useful if you want to execute operations on global variables.
+It makes sure that only 1 bot can enter a given piece of code at a time, so that multiple bots do not edit the same global variable at the same time.
+Often used in conjunction with TRY/CATCH.
+Example:
+```
+LOCK globals
+TRY
+// Try to increase globals.Count by 1 if it exists
+globals.Count++;
+CATCH
+// Create globals.Count if it doesn't exist
+globals.Count = 1;
+END
+END
+```
+---
+##### SET VAR/CAP
+Sets a string variable, and optionally also marks it for capture. Introduced for consistency with OB1.
+Example:
+```
+SET VAR myString "variable"
+LOG myString
+
+SET CAP myCapture "capture"
+LOG myCapture
+```
---
+++ wwwroot/_content/GridBlazor/js/gridblazor.js
@@ -0,0 +1,84 @@
+﻿window.gridJsFunctions = {
+    focusElement: function (element) {
+        if(element)
+            element.focus();
+    },
+    isDateTimeLocalSupported: function () {
+        var elem = document.createElement("input");
+        elem.setAttribute("type", "datetime-local");
+        return elem.type !== "text";
+    },
+    isWeekSupported: function () {
+        var elem = document.createElement("input");
+        elem.setAttribute("type", "week");
+        return elem.type !== "text";
+    },
+    isMonthSupported: function () {
+        var elem = document.createElement("input");
+        elem.setAttribute("type", "month");
+        return elem.type !== "text";
+    },
+    getPosition: function (element) {
+        if (element) {
+            var width = element.offsetWidth;
+            var height = element.offsetHeight;
+            var x = 0;
+            var y = 0;
+            while (element && !isNaN(element.offsetLeft) && !isNaN(element.offsetTop)) {
+                x += element.offsetLeft - element.scrollLeft;
+                y += element.offsetTop - element.scrollTop;
+                element = element.offsetParent;
+            }
+            x -= Math.round(window.pageXOffset);
+            y -= Math.round(window.pageYOffset);
+            var screenHeight = screen.availHeight;
+            var screenWidth = screen.availWidth;
+            var innerHeight = window.innerHeight;
+            var innerWidth = window.innerWidth;
+            return {
+                Width: Math.round(width), Height: Math.round(height), X: Math.round(x), Y: Math.round(y),
+                ScreenWidth: Math.round(screenWidth), ScreenHeight: Math.round(screenHeight),
+                InnerWidth: Math.round(innerWidth), InnerHeight: Math.round(innerHeight)
+            };
+        }
+        else
+            return null;
+    },
+    setActive: function (element, i) {
+        if (element) {
+            var elements = element.querySelectorAll('[data-grid="tab"]');
+            [].forEach.call(elements, function (el) {
+                el.classList.remove("active");
+            });
+            elements = element.querySelectorAll('[data-grid-id="' + i + '"]');
+            [].forEach.call(elements, function (el) {
+                el.classList.add("active");
+            });
+        }
+    },
+    saveAsFile: function (filename, bytesBase64) {
+        var link = document.createElement('a');
+        link.download = filename;
+        link.href = "data:application/octet-stream;base64," + bytesBase64;
+        document.body.appendChild(link); // Needed for Firefox
+        link.click();
+        document.body.removeChild(link);
+    },
+    click: function (element) {
+        if (element)
+            element.click();
+    },
+    showElement: function (element) {
+        if (element)
+            element.style.display = "block";
+    },
+    hideElement: function (element) {
+        if (element)
+            element.style.display = "none";
+    },
+    scrollFixedSizeTable: function (gridTableHead, gridTableBody) {
+        document.getElementById(gridTableBody).onscroll = function () {
+            document.getElementById(gridTableHead).scrollLeft = this.scrollLeft;
+        };
+    }
+}
